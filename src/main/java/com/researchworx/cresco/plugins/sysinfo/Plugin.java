d23de6137ff9642fefd7f7431f27b9d3f0ba4b3e
Cody Bumgardner <cody@uky.edu>
2016-05-14 14:24:34 -0400

    Adding watchdog

--- src/main/java/com/researchworx/cresco/plugins/sysinfo/Plugin.java
+++ src/main/java/com/researchworx/cresco/plugins/sysinfo/Plugin.java
@@ -7,8 +7,7 @@ import com.researchworx.cresco.library.plugin.core.CPlugin;
 @AutoService(CPlugin.class)
 public class Plugin extends CPlugin {
     private PerfMonitor perfMonitor;
-    private WatchDog watchMonitor;
-
+    
     public void setExecutor() {
         this.exec = new Executor(this);
     }
