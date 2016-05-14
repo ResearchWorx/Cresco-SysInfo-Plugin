1990a6d63a7d24962c7dfea9219de4b3ba1aae38
Cody Bumgardner <cody@uky.edu>
2016-05-14 12:14:43 -0400

    Adding watchdog

--- src/main/java/com/researchworx/cresco/plugins/sysinfo/Plugin.java
+++ src/main/java/com/researchworx/cresco/plugins/sysinfo/Plugin.java
@@ -16,9 +16,6 @@ public class Plugin extends CPlugin {
     public void start() {
         perfMonitor = new PerfMonitor(this);
         perfMonitor.start();
-        startWatchDog();
-
-
 
     }
 }
