8e3a03fca1fb0ccb35ec33456fe0273191a22fb9
Caylin Hickey <caylin.hickey@uky.edu>
2016-05-21 18:50:50 -0400

    Removed basically empty Executor Added call to cleanUp to shutdown timer to prevent messages happening after shutdown Updated Cresco-Plugin-Library version to 0.4.6

--- src/main/java/com/researchworx/cresco/plugins/sysinfo/Plugin.java
+++ src/main/java/com/researchworx/cresco/plugins/sysinfo/Plugin.java
@@ -1,19 +1,19 @@
 package com.researchworx.cresco.plugins.sysinfo;
 
 import com.google.auto.service.AutoService;
-import com.researchworx.cresco.library.core.WatchDog;
 import com.researchworx.cresco.library.plugin.core.CPlugin;
 
 @AutoService(CPlugin.class)
 public class Plugin extends CPlugin {
     private PerfMonitor perfMonitor;
-    
-    public void setExecutor() {
-        this.exec = new Executor(this);
-    }
 
     public void start() {
         perfMonitor = new PerfMonitor(this);
         perfMonitor.start();
+    }
+
+    @Override
+    public void cleanUp() {
+        perfMonitor.stop();
     }
 }
