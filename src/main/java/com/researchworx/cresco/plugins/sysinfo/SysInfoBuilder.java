d8c6d70885d8368ec50868cd920d72e796f1455f
Cody Bumgardner <cody@uky.edu>
2017-01-24 15:59:49 -0500

    updated oshi version.  Still Java7 works with Java 7

--- src/main/java/com/researchworx/cresco/plugins/sysinfo/SysInfoBuilder.java
+++ src/main/java/com/researchworx/cresco/plugins/sysinfo/SysInfoBuilder.java
@@ -1,9 +1,10 @@
 package com.researchworx.cresco.plugins.sysinfo;
 
 import oshi.SystemInfo;
+import oshi.hardware.HWDiskStore;
 import oshi.hardware.HardwareAbstractionLayer;
 import oshi.software.os.OSFileStore;
-import oshi.util.FormatUtil;
+import oshi.software.os.OperatingSystem;
 
 import java.net.InetAddress;
 import java.net.InterfaceAddress;
@@ -13,59 +14,40 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 class SysInfoBuilder {
-    private static ConcurrentHashMap<String, String> info = new ConcurrentHashMap<>();
+    private ConcurrentHashMap<String, String> info = new ConcurrentHashMap<>();
+    private SystemInfo systemInfo;
+    private HardwareAbstractionLayer hardwareAbstractionLayer;
+    private OperatingSystem os;
 
-    SysInfoBuilder() {
+    public Map<String, String> getSysInfoMap() {
+
+        try {
+            systemInfo = new SystemInfo();
+            hardwareAbstractionLayer = systemInfo.getHardware();
+            os = systemInfo.getOperatingSystem();
+
+
+            getOSInfo();
+            getFSInfo();
+            getSensorInfo();
+            getDiskInfo();
+            getCPUInfo();
+            getMemoryInfo();
+            getNetworkInfo();
+
+
+        } catch (Exception e) {
+            System.out.println("SysInfoBuilder : getSysInfoMap : Error : " + e.getMessage());
+            e.printStackTrace();
+        }
+        return info;
+    }
+
+    public SysInfoBuilder() {
+        /*
         System.setProperty("java.net.preferIPv4Stack", "true");
         try {
-            SystemInfo systemInfo = new SystemInfo();
-            HardwareAbstractionLayer hardwareAbstractionLayer = systemInfo.getHardware();
 
-            info.put("sys-os", systemInfo.getOperatingSystem().toString());
-            info.put("cpu-core-count", String.valueOf(hardwareAbstractionLayer.getProcessors().length));
-            info.put("cpu-sn", hardwareAbstractionLayer.getProcessors()[0].getSystemSerialNumber());
-            info.put("cpu-summary", hardwareAbstractionLayer.getProcessors()[0].toString());
-
-            try {
-                info.put("cpu-ident", hardwareAbstractionLayer.getProcessors()[0].getIdentifier());
-            }
-            catch (Exception ex) {
-                info.put("cpu-ident", "unknown");
-            }
-            try {
-                info.put("cpu-sn-ident",hardwareAbstractionLayer.getProcessors()[0].getIdentifier());
-            }
-            catch(Exception ex) {
-                info.put("cpu-sn-ident","unknown");
-            }
-            info.put("memory-total", String.valueOf(hardwareAbstractionLayer.getMemory().getTotal()));
-            int nicCount = 0;
-            StringBuilder nicStringBuilder = new StringBuilder();
-            try {
-                Enumeration<NetworkInterface> nicEnum = NetworkInterface.getNetworkInterfaces();
-                while (nicEnum.hasMoreElements()) {
-                    NetworkInterface nic = nicEnum.nextElement();
-                    if (nic.isLoopback())
-                        continue;
-                    nicStringBuilder.append(String.valueOf(nicCount)).append(":").append(nic.getName()).append(",");
-                    StringBuilder interfaceAddressStringBuilder = new StringBuilder();
-                    for (InterfaceAddress interfaceAddress : nic.getInterfaceAddresses()) {
-                        if (interfaceAddress == null)
-                            continue;
-                        try {
-                            InetAddress address = interfaceAddress.getAddress();
-                            interfaceAddressStringBuilder.append(address.getHostAddress()).append(",");
-                        } catch (Exception e) {
-                            System.out.println("SysInfoBuilder : Constructor : nicLoop : addrLoop : Error : " + e.getMessage());
-                        }
-                    }
-                    if (interfaceAddressStringBuilder.length() == 0)
-                        continue;
-                    interfaceAddressStringBuilder.deleteCharAt(interfaceAddressStringBuilder.lastIndexOf(","));
-                    info.put("nic-" + String.valueOf(nicCount) + "-ip", interfaceAddressStringBuilder.toString());
-                }
-                nicStringBuilder.deleteCharAt(nicStringBuilder.lastIndexOf(","));
-                info.put("nic-map", nicStringBuilder.toString());
             } catch (Exception e) {
                 System.out.println("SysInfoBuilder : Constructor : nicLoop : Error : " + e.getMessage());
             }
@@ -74,40 +56,14 @@ class SysInfoBuilder {
             System.out.println("SysInfoBuilder : Constructor : Error : " + e.getMessage());
             e.printStackTrace();
         }
+        */
     }
 
-    Map<String, String> getSysInfoMap() {
-        try {
-            SystemInfo systemInfo = new SystemInfo();
-            HardwareAbstractionLayer hardwareAbstractionLayer = systemInfo.getHardware();
-
-            info.put("sys-uptime", FormatUtil.formatElapsedSecs(hardwareAbstractionLayer.getProcessors()[0].getSystemUptime()));
-            info.put("memory-available", String.valueOf(hardwareAbstractionLayer.getMemory().getAvailable()));
-
-            long[] prevTicks = hardwareAbstractionLayer.getProcessors()[0].getSystemCpuLoadTicks();
-            Thread.sleep(1000);
-            long[] ticks = hardwareAbstractionLayer.getProcessors()[0].getSystemCpuLoadTicks();
-
-            long user = ticks[0] - prevTicks[0];
-            long nice = ticks[1] - prevTicks[1];
-            long sys = ticks[2] - prevTicks[2];
-            long idle = ticks[3] - prevTicks[3];
-            long totalCpu = user + nice + sys + idle;
-
-            StringBuilder procCpu = new StringBuilder("CPU Load per processor:");
-            for (int cpu = 0; cpu < hardwareAbstractionLayer.getProcessors().length; cpu++)
-                procCpu.append(String.format(" %.1f%%", hardwareAbstractionLayer.getProcessors()[cpu].getProcessorCpuLoadBetweenTicks() * 100));
-
-
-            info.put("cpu-user-load", String.valueOf(100d * user / totalCpu));
-            info.put("cpu-nice-load", String.valueOf(100d * nice / totalCpu));
-            info.put("cpu-sys-load", String.valueOf(100d * sys / totalCpu));
-            info.put("cpu-idle-load", String.valueOf(100d * idle / totalCpu));
-            info.put("cpu-per-cpu-load", procCpu.toString());
-
+    public void getFSInfo() {
+        try{
             int fsCount = 0;
             StringBuilder fsStringBuilder = new StringBuilder();
-            OSFileStore[] fsArray = hardwareAbstractionLayer.getFileStores();
+            OSFileStore[] fsArray = hardwareAbstractionLayer.getFileSystem().getFileStores();
             for (OSFileStore fs : fsArray) {
                 fsStringBuilder.append(String.valueOf(fsCount)).append(":").append(fs.getName()).append(",");
                 long usable = fs.getUsableSpace();
@@ -116,16 +72,214 @@ class SysInfoBuilder {
                 info.put("fs-" + String.valueOf(fsCount) + "-total", String.valueOf(total));
                 fsCount++;
             }
-            fsStringBuilder.deleteCharAt(fsStringBuilder.lastIndexOf(","));
+            int index = fsStringBuilder.lastIndexOf(",");
+            if(index != -1) {
+                fsStringBuilder.deleteCharAt(index);
+            }
             info.put("fs-map", fsStringBuilder.toString());
-        } catch (Exception e) {
-            System.out.println("SysInfoBuilder : getSysInfoMap : Error : " + e.getMessage());
-            e.printStackTrace();
         }
-        return info;
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
-    static ConcurrentHashMap<String, String> getInfo() {
+    public void getOSInfo() {
+        try{
+
+            info.put("sys-os", os.getVersion().toString());
+            info.put("sys-family", os.getFamily());
+            //info.put("sys-threadcount", String.valueOf(os.getThreadCount()));
+            info.put("sys-manufacturer", os.getManufacturer());
+            //info.put("sys-uptime", FormatUtil.formatElapsedSecs(hardwareAbstractionLayer.getProcessor().getSystemUptime()));
+            info.put("sys-uptime", String.valueOf(hardwareAbstractionLayer.getProcessor().getSystemUptime()));
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void getSensorInfo() {
+        try{
+
+            info.put("sys-temperature", String.format("%.1f", hardwareAbstractionLayer.getSensors().getCpuTemperature()));
+            info.put("sys-voltage", String.format("%.1f",hardwareAbstractionLayer.getSensors().getCpuVoltage()));
+            StringBuilder fanStringBuilder = new StringBuilder();
+            for(int fanspeed : hardwareAbstractionLayer.getSensors().getFanSpeeds()) {
+                fanStringBuilder.append(String.valueOf(fanspeed)).append(",");
+            }
+
+            int index = fanStringBuilder.lastIndexOf(",");
+            if(index != -1) {
+                fanStringBuilder.deleteCharAt(index);
+            }
+
+            info.put("sys-fanspeeds", fanStringBuilder.toString());
+
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void getDiskInfo() {
+        try{
+
+            /*
+            int fsCount = 0;
+            StringBuilder fsStringBuilder = new StringBuilder();
+            HWDiskStore[] fsArray = hardwareAbstractionLayer.getDiskStores();
+            for (HWDiskStore fs : fsArray) {
+                fsStringBuilder.append(String.valueOf(fsCount)).append(":").append(fs.getName()).append(",");
+                long usable = fs.get .getSize()fs.getUsableSpace();
+                long total = fs.getTotalSpace();
+                info.put("fs-" + String.valueOf(fsCount) + "-available", String.valueOf(usable));
+                info.put("fs-" + String.valueOf(fsCount) + "-total", String.valueOf(total));
+                fsCount++;
+            }
+            fsStringBuilder.deleteCharAt(fsStringBuilder.lastIndexOf(","));
+            info.put("fs-map", fsStringBuilder.toString());
+            */
+            int diskCount = 0;
+            StringBuilder diskStringBuilder = new StringBuilder();
+            HWDiskStore[] diskArray = hardwareAbstractionLayer.getDiskStores();
+            for (HWDiskStore disk : diskArray) {
+                diskStringBuilder.append(String.valueOf(diskCount)).append(":").append(disk.getName()).append(",");
+                /*
+                int partCount = 0;
+
+                for(HWPartition part : disk.getPartitions()) {
+                    info.put("disk-" + String.valueOf(diskCount) + "-part-" + String.valueOf(partCount) + "-CODY", String.valueOf(part.getIdentification()));
+                    info.put("disk-" + String.valueOf(diskCount) + "-part-" + String.valueOf(partCount) + "-CODY", String.valueOf(part.getName()));
+                    info.put("disk-" + String.valueOf(diskCount) + "-part-" + String.valueOf(partCount) + "-CODY", String.valueOf(part.getSize()));
+                    info.put("disk-" + String.valueOf(diskCount) + "-part-" + String.valueOf(partCount) + "-CODY", String.valueOf(part.getMountPoint()));
+
+                    partCount++;
+                }
+                */
+                info.put("disk-" + String.valueOf(diskCount) + "-size", String.valueOf(disk.getSize()));
+                info.put("disk-" + String.valueOf(diskCount) + "-model", String.valueOf(disk.getModel()));
+                info.put("disk-" + String.valueOf(diskCount) + "-name", String.valueOf(disk.getName()));
+                //info.put("disk-" + String.valueOf(diskCount) + "-readbytes", String.valueOf(disk.getReadBytes()));
+                //info.put("disk-" + String.valueOf(diskCount) + "-writebytes", String.valueOf(disk.getWriteBytes()));
+
+                //long usable = fs.get .getSize()fs.getUsableSpace();
+                //long total = fs.getTotalSpace();
+                //info.put("fs-" + String.valueOf(fsCount) + "-available", String.valueOf(usable));
+                //info.put("fs-" + String.valueOf(fsCount) + "-total", String.valueOf(total));
+                diskCount++;
+            }
+
+            int index = diskStringBuilder.lastIndexOf(",");
+            if(index != -1) {
+                diskStringBuilder.deleteCharAt(index);
+            }
+
+            info.put("disk-map", diskStringBuilder.toString());
+
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void getMemoryInfo() {
+        try{
+            info.put("memory-total", String.valueOf(hardwareAbstractionLayer.getMemory().getTotal()));
+            info.put("memory-available", String.valueOf(hardwareAbstractionLayer.getMemory().getAvailable()));
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void getNetworkInfo() {
+        try{
+            int nicCount = 0;
+            StringBuilder nicStringBuilder = new StringBuilder();
+            //try {
+            Enumeration<NetworkInterface> nicEnum = NetworkInterface.getNetworkInterfaces();
+            while (nicEnum.hasMoreElements()) {
+                NetworkInterface nic = nicEnum.nextElement();
+
+                if (nic.isLoopback())
+                    continue;
+                nicStringBuilder.append(String.valueOf(nicCount)).append(":").append(nic.getName()).append(",");
+                StringBuilder interfaceAddressStringBuilder = new StringBuilder();
+                for (InterfaceAddress interfaceAddress : nic.getInterfaceAddresses()) {
+                    if (interfaceAddress == null)
+                        continue;
+                    try {
+                        InetAddress address = interfaceAddress.getAddress();
+                        interfaceAddressStringBuilder.append(address.getHostAddress()).append(",");
+                    } catch (Exception e) {
+                        System.out.println("SysInfoBuilder : Constructor : nicLoop : addrLoop : Error : " + e.getMessage());
+                    }
+                }
+                if (interfaceAddressStringBuilder.length() == 0)
+                    continue;
+                interfaceAddressStringBuilder.deleteCharAt(interfaceAddressStringBuilder.lastIndexOf(","));
+                info.put("nic-" + String.valueOf(nicCount) + "-ip", interfaceAddressStringBuilder.toString());
+                info.put("nic-" + String.valueOf(nicCount) + "-mtu", String.valueOf(nic.getMTU()));
+
+            }
+            int index = nicStringBuilder.lastIndexOf(",");
+            if(index != -1) {
+                nicStringBuilder.deleteCharAt(index);
+            }
+
+            info.put("nic-map", nicStringBuilder.toString());
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void getCPUInfo() {
+        try{
+
+            info.put("is64bit", String.valueOf(hardwareAbstractionLayer.getProcessor().isCpu64bit()));
+            info.put("cpu-physical-count", String.valueOf(hardwareAbstractionLayer.getProcessor().getPhysicalProcessorCount()));
+            info.put("cpu-logical-count", String.valueOf(hardwareAbstractionLayer.getProcessor().getLogicalProcessorCount()));
+            info.put("cpu-sn", hardwareAbstractionLayer.getProcessor().getSystemSerialNumber());
+            info.put("cpu-summary", hardwareAbstractionLayer.getProcessor().toString());
+
+            try {
+                info.put("cpu-ident", hardwareAbstractionLayer.getProcessor().getIdentifier());
+            }
+            catch (Exception ex) {
+                info.put("cpu-ident", "unknown");
+            }
+            try {
+                info.put("cpu-sn-ident",hardwareAbstractionLayer.getProcessor().getIdentifier());
+            }
+            catch(Exception ex) {
+                info.put("cpu-sn-ident","unknown");
+            }
+
+            //performance
+            long[] prevTicks = hardwareAbstractionLayer.getProcessor().getSystemCpuLoadTicks();
+            Thread.sleep(1000);
+            long[] ticks = hardwareAbstractionLayer.getProcessor().getSystemCpuLoadTicks();
+
+            long user = ticks[0] - prevTicks[0];
+            long nice = ticks[1] - prevTicks[1];
+            long sys = ticks[2] - prevTicks[2];
+            long idle = ticks[3] - prevTicks[3];
+            long totalCpu = user + nice + sys + idle;
+
+            info.put("cpu-user-load", String.format("%.1f", (100d * user / totalCpu)));
+            info.put("cpu-nice-load", String.format("%.1f", (100d * nice / totalCpu)));
+            info.put("cpu-sys-load", String.format("%.1f", (100d * sys / totalCpu)));
+            info.put("cpu-idle-load", String.format("%.1f", (100d * idle / totalCpu)));
+
+
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public ConcurrentHashMap<String, String> getInfo() {
         return info;
     }
 }
