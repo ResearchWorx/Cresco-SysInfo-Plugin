8c0f303066c5a04f8e5689a4b38bb7a3041138e4
Cody Bumgardner <cody@uky.edu>
2017-01-27 06:03:41 -0500

    removed sensor query due to error on mac

--- src/main/java/com/researchworx/cresco/plugins/sysinfo/PerfMonitor.java
+++ src/main/java/com/researchworx/cresco/plugins/sysinfo/PerfMonitor.java
@@ -19,7 +19,7 @@ class PerfMonitor {
         this.plugin = plugin;
         builder = new SysInfoBuilder();
         bmark = new Benchmark();
-        bm = bmark.bench();
+        //bm = bmark.bench();
         //System.out.println("Benchmark Runtime: " + bm.getRunTime() + " CPU Performance: " + bm.getCPU());
 
 
@@ -71,7 +71,9 @@ class PerfMonitor {
             tick.setParam("inode_id",plugin.getConfig().getStringParam("inode_id","sysinfo_inode"));
             //tick.setParam("resource_id","sysinfo_resource");
             //tick.setParam("inode_id","sysinfo_inode");
-            tick.setParam("benchmark_cpu_composite",String.valueOf((int)bm.getCPU()));
+            //tick.setParam("benchmark_cpu_composite",String.valueOf((int)bm.getCPU()));
+            tick.setParam("benchmark_cpu_composite","1000");
+
             for(Map.Entry<String, String> entry : builder.getSysInfoMap().entrySet()) {
                 tick.setParam(entry.getKey(), entry.getValue());
             }
--- src/main/java/com/researchworx/cresco/plugins/sysinfo/SysInfoBuilder.java
+++ src/main/java/com/researchworx/cresco/plugins/sysinfo/SysInfoBuilder.java
@@ -29,7 +29,7 @@ class SysInfoBuilder {
 
             getOSInfo();
             getFSInfo();
-            getSensorInfo();
+            //getSensorInfo();
             //getDiskInfo();
             getCPUInfo();
             getMemoryInfo();
